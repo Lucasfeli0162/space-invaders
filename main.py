b7805a697de7cd8db20d48346134b72956049e8c
Lucas Feitosa <universenerd249@gmail.com>
2022-01-03 23:15:07 -0300

    adicionei o player e tentei adicionar o tiro

--- main.py
+++ main.py
@@ -18,6 +18,56 @@ running = True
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 50
+        self.height = 50
+        self.player = self.render()
+        self.attacking = False
+        self.tiro = None
+
+    def update(self):
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[K_RIGHT]:
+            self.x += 0.5
+        if keys_pressed[K_LEFT]:
+            self.x -= 0.5
+        if keys_pressed[K_UP]:
+            self.attacking = True
+            shoot = Shoot()
+        if self.attacking:
+            shoot.update()
+
+    def render(self):
+        medium_point = (self.x, self.y)
+        left_point = (self.x - self.width / 2, self.y + self.height)
+        right_point = (self.x + self.width / 2, self.y + self.height)
+        triangle = pygame.draw.polygon(SCREEN, WHITE, (left_point, medium_point, right_point))
+        return triangle
+
+
+class Shoot:
+    def __init__(self):
+        self.radius = 5
+        self.x = 0
+        self.y = 0
+        self.tiro = None
+
+    def attack(self):
+        initial_point = (player.x, player.y)
+        self.y = player.y
+        self.x = player.x
+        self.tiro = pygame.draw.circle(SCREEN, WHITE, initial_point, 5)
+
+    def update(self):
+        self.y -= 1
+
+
+player = Player(WIDTH / 2 - 25, HEIGHT - 50)
+
 while running:
 
     SCREEN.fill(BLACK)
@@ -25,6 +75,9 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+
+    player.update()
+    player.render()
 
     pygame.display.flip()
 
